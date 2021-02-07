095bd233cc9f5eed71ab479523e81d464b41eba0
codertylerasai@gmail.com <codertylerasai@gmail.com>
2021-02-06 17:57:33 -0700

    practicing maps

--- hello.go
+++ hello.go
@@ -5,7 +5,12 @@ import (
 )
 
 func main() {
-	a := []int{5, 4, 3, 2, 1}
-	a = append(a, 13)
-	fmt.Println(a)
+	vertices := make(map[string]int)
+
+	vertices["triangle"] = 2
+	vertices["square"] = 3
+	vertices["dodecagon"] = 12
+
+	fmt.Println(vertices["triangle"])
+
 }
