cd37c949c80fca2ea9328633c96684ab222e8e74
codertylerasai@gmail.com <codertylerasai@gmail.com>
2021-02-06 17:51:31 -0700

    first lines of Go

---
+++ hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 5
+	y := 7
+	sum := x + y
+	fmt.Println(sum)
+}
+
+func ifStatement() {
+	x := 7
+
+	if x > 6 {
+		fmt.Println("more than 6")
+	} else if x < 2 {
+
+	} else {
+
+	}
+}
