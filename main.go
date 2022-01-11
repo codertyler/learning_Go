f8a2eb5106b3ae2182d006efb167529d0e384c15
Tyler Asai <tyler.asai@agilebits.com>
2022-01-10 20:56:57 -0700

    Create http server

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page")
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
+
 func main() {
 
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	_ = http.ListenAndServe(":8081", nil)
 }
